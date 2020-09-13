17abfdc2c6a1058722baad0401225ad061763402
xtophe xanon <xocon10@gmail.com>
2020-09-13 14:02:22 -0500

    add comment functionality

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,21 +1,44 @@
 const { Schema, model } = require('mongoose');
+const moment = require('moment');
 
-const PizzaSchema = new Schema({
-  pizzaName: {
-    type: String
-  },
-  createdBy: {
-    type: String
-  },
-  createdAt: {
-    type: Date, 
-    default: Date.now
-  },
-  size: {
-    type: String,
-    default: 'Large'
-  }, 
-  toppings: []
+const PizzaSchema = new Schema(
+	{
+		pizzaName : {
+			type : String
+		},
+		createdBy : {
+			type : String
+		},
+		createdAt : {
+			type    : Date,
+      default : Date.now,
+      get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+		},
+		size      : {
+			type    : String,
+			default : 'Large'
+		},
+		toppings  : [],
+		// create association with Comment model
+		comments  : [
+			{
+				type : Schema.Types.ObjectId,
+				ref  : 'Comment'
+			}
+		]
+	},
+	{
+		toJSON : {
+      virtuals : true,
+      getters: true
+		},
+		id     : false
+	}
+);
+
+// get total count of comments and replies on retrieval -- virtual property
+PizzaSchema.virtual('commentCount').get(function() {
+	return this.comments.length;
 });
 
 // create the Pizza model using the PizzaSchema
